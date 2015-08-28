af6a19953e148ba57e07b6ea7026704da3029589
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-28 02:00:32 -0700

    create the tables for the database

--- app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitDbHelper.java
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitDbHelper.java
@@ -12,21 +12,27 @@ public class HabitDbHelper extends SQLiteOpenHelper {
 
     static final String DATABASE_NAME = "habitrevamp.db";
 
-    final String SQL_CREATE_HABIT_TABLE = "CREATE TABLE " + HabitContract.HabitEntry.TABLE_NAME + " (" +
-            HabitContract.HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            HabitContract.HabitEntry.COLUMN_TITLE + " TEXT, " +
-            HabitContract.HabitEntry.COLUMN_START_DATE + " INTEGER " +
-            ");";
-
-    final String SQL_CREATE_HABIT_COMPLETED_TABLE = "CREATE TABLE " + HabitContract.HabitDayCompleteEntry.TABLE_NAME + " (" +
-            HabitContract.HabitDayCompleteEntry._ID + " INTEGER PRIMARY KEY, " +
-            HabitContract.HabitDayCompleteEntry.COLUMN_HABIT_ID + " INTEGER, " +
-            HabitContract.HabitDayCompleteEntry.COLUMN_DATE + " INTEGER, " +
-            HabitContract.HabitDayCompleteEntry.COLUMN_COMPLETE + " INTEGER " +
-            ");";
-
     public HabitDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_HABIT_TABLE = "CREATE TABLE " + HabitContract.HabitEntry.TABLE_NAME + " (" +
+                HabitContract.HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                HabitContract.HabitEntry.COLUMN_TITLE + " TEXT, " +
+                HabitContract.HabitEntry.COLUMN_START_DATE + " INTEGER " +
+                ");";
+
+        final String SQL_CREATE_HABIT_COMPLETED_TABLE = "CREATE TABLE " + HabitContract.HabitDayCompleteEntry.TABLE_NAME + " (" +
+                HabitContract.HabitDayCompleteEntry._ID + " INTEGER PRIMARY KEY, " +
+                HabitContract.HabitDayCompleteEntry.COLUMN_HABIT_ID + " INTEGER, " +
+                HabitContract.HabitDayCompleteEntry.COLUMN_DATE + " INTEGER, " +
+                HabitContract.HabitDayCompleteEntry.COLUMN_COMPLETE + " INTEGER " +
+                ");";
+        
+        db.execSQL(SQL_CREATE_HABIT_TABLE);
+        db.execSQL(SQL_CREATE_HABIT_COMPLETED_TABLE);
     }
 
     @Override
@@ -34,8 +40,4 @@ public class HabitDbHelper extends SQLiteOpenHelper {
 
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase sb) {
-
-    }
 }
