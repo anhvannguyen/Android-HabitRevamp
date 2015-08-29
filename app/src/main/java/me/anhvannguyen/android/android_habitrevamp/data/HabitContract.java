7f34d7c104a645154d82bc5ce0e0fcba00942d6e
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-28 19:02:01 -0700

    add constants for content provider

--- app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitContract.java
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitContract.java
@@ -1,5 +1,8 @@
 package me.anhvannguyen.android.android_habitrevamp.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +10,22 @@ import android.provider.BaseColumns;
  */
 public class HabitContract {
 
+    public static final String CONTENT_AUTHORITY = "me.anhvannguyen.android.android_habitrevamp";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_HABIT = "habit";
+    public static final String PATH_HABIT_DAY = "habitday";
+
     public static final class HabitEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_HABIT).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HABIT;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HABIT;
+
         public static final String TABLE_NAME = "habits";
 
         // title of habit user wish to achieve
@@ -16,9 +34,26 @@ public class HabitContract {
         // date of when the user want to start
         public static final String COLUMN_START_DATE = "start_date";
 
+        // content://me.anhvannguyen.android.android_habitrevamp/habit/{id}
+        public static String getHabitId(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static Uri buildHabitUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
     }
 
     public static final class HabitDayCompleteEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_HABIT_DAY).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HABIT_DAY;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_HABIT_DAY;
+
         public static final String TABLE_NAME = "complete_days";
 
         // HabitEntry _id value
@@ -29,6 +64,10 @@ public class HabitContract {
 
         // check if user complete the habit on the day date
         public static final String COLUMN_COMPLETE = "complete";
+
+        public static Uri buildHabitDayUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
 
     }
 }
