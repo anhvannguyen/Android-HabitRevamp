3350f088305cfe271eb7e1e9ddd351b4862c6a32
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-30 15:56:40 -0700

    get reference to the views

--- app/src/main/java/me/anhvannguyen/android/android_habitrevamp/NewHabitFragment.java
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/NewHabitFragment.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewHabitFragment extends Fragment {
-
+    private EditText mTitleEditText;
+    private EditText mSubtitleEditText;
+    private Button mStartDateButton;
+    private TextView mStartDateTextView;
+    private Button mSaveButton;
 
     public NewHabitFragment() {
         // Required empty public constructor
@@ -22,7 +29,31 @@ public class NewHabitFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_habit, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_new_habit, container, false);
+
+        mTitleEditText = (EditText) rootView.findViewById(R.id.habit_title_edittext);
+
+        mSubtitleEditText = (EditText) rootView.findViewById(R.id.habit_subtitle_edittext);
+
+        mStartDateButton = (Button) rootView.findViewById(R.id.habit_select_date_button);
+        mStartDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        mStartDateTextView = (TextView) rootView.findViewById(R.id.habit_start_date_textview);
+
+        mSaveButton = (Button) rootView.findViewById(R.id.habit_save_button);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return rootView;
     }
 
 
