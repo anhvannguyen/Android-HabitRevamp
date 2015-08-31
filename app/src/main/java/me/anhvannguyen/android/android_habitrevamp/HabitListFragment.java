533a67b56b29c23937a4fd1d2dd7af54a7bcc98e
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-31 16:31:09 -0700

    switch to support fragment and use loader to query data

--- app/src/main/java/me/anhvannguyen/android/android_habitrevamp/HabitListFragment.java
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/HabitListFragment.java
@@ -2,26 +2,34 @@ package me.anhvannguyen.android.android_habitrevamp;
 
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import me.anhvannguyen.android.android_habitrevamp.data.HabitContract;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HabitListFragment extends Fragment {
+public class HabitListFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int HABIT_LIST_LOADER = 0;
 
     private RecyclerView mHabitListRecyclerView;
     private HabitRecyclerAdapter mHabitRecyclerAdapter;
     private FloatingActionButton mAddNewHabitButton;
     private CoordinatorLayout mCoordinatorLayout;
+
 
     public HabitListFragment() {
         // Required empty public constructor
@@ -36,8 +44,11 @@ public class HabitListFragment extends Fragment {
 
         mCoordinatorLayout = (CoordinatorLayout) rootView.findViewById(R.id.coordinator_layout);
 
+        mHabitRecyclerAdapter = new HabitRecyclerAdapter(getActivity());
+
         mHabitListRecyclerView = (RecyclerView) rootView.findViewById(R.id.main_habit_recyclerview);
         mHabitListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mHabitListRecyclerView.setAdapter(mHabitRecyclerAdapter);
 
         mAddNewHabitButton = (FloatingActionButton) rootView.findViewById(R.id.add_fab);
         mAddNewHabitButton.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +62,37 @@ public class HabitListFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(HABIT_LIST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(HABIT_LIST_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getActivity(),
+                HabitContract.HabitEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mHabitRecyclerAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mHabitRecyclerAdapter.swapCursor(null);
+    }
 }
