0bb5cea35624ca230527dd203702af061a787669
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-09-02 01:22:03 -0700

    create the detail activity

---
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/HabitDetailActivity.java
@@ -0,0 +1,43 @@
+package me.anhvannguyen.android.android_habitrevamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HabitDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_detail);
+
+        HabitDetailFragment fragment = new HabitDetailFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.habit_detail_container, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_habit_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
