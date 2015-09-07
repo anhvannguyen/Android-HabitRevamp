2a30cd47f4365c5b53697fcb2c35935352655376
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-09-07 16:41:46 -0700

    implement bulk insert for content provider

--- app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitContentProvider.java
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/data/HabitContentProvider.java
@@ -278,6 +278,56 @@ public class HabitContentProvider extends ContentProvider {
     }
 
     @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case HABIT: {
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(
+                                HabitContract.HabitEntry.TABLE_NAME,
+                                null,
+                                value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            }
+            case DATE: {
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(
+                                HabitContract.DayCompleteEntry.TABLE_NAME,
+                                null,
+                                value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            }
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
+    @Override
     public void shutdown() {
         mOpenHelper.close();
         super.shutdown();
