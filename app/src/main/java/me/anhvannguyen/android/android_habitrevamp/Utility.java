348bfd24fcb9b60bb5ecdf4159139b9fd8646cfe
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-09-12 19:37:51 -0700

    create a utility class for constants and helpers

---
+++ app/src/main/java/me/anhvannguyen/android/android_habitrevamp/Utility.java
@@ -0,0 +1,16 @@
+package me.anhvannguyen.android.android_habitrevamp;
+
+import android.content.ContentValues;
+
+/**
+ * Created by anhvannguyen on 9/12/15.
+ */
+public class Utility {
+
+    public static ContentValues createHabitTestValue() {
+        ContentValues values = new ContentValues();
+
+
+        return values;
+    }
+}
